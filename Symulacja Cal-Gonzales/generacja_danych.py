89b8d3a120e5da2a9e5ef9c983913a1533181207
Ania Gieb <a.giebult@gmail.com>
2026-04-06 20:55:11 +0200

    poprawa na plik Python

---
+++ Symulacja Cal-Gonzales/generacja_danych.py
@@ -0,0 +1,102 @@
+## poprawka na średnice
+import numpy as np
+import pandas as pd
+
+# -------------------------------------------------------
+# Zmienne i funkcje fizyczne
+# -------------------------------------------------------
+m_e = 0.511                 # masa elektronu w [MeV/c2]
+rho_water = 1.0           # gęstość wody [g/cm^3]
+rho_plastic = 1.18        # gęstość plastiku (np. PMMA/Akryl) [g/cm^3]
+wall_thickness_mm = 1.0   # Grubość ścianki w [mm] (zmień na 10.0 jeśli serio miały to być "cm")
+
+isotopes = {"18F": (0.634, 8), "44Sc": (1.474, 20)} # max energia emisji, liczba atomowa isotopu <<< DO SPRAWDZENIA :)
+diameters_mm = [10, 13, 17, 22, 28, 37] # Średnice WEWNĘTRZNE (woda)
+N_sim = 100
+
+def momentum(E): # założenie: liczymy tylko dla elektronu
+    return np.sqrt(E**2 + 2 * m_e * E) # liczenie pędu
+
+def fermi_function_allowed(Z, E):  
+    alpha = 1/137
+    p = momentum(E)
+    p = np.where(p==0, 1e-9, p)
+    eta = alpha * Z * (E + m_e) / p
+    return (2 * np.pi * eta) / (1 - np.exp(-2 * np.pi * eta))
+
+def beta_plus_spectrum(E, E0, Z):  # (energia, energia maksymalna, liczba atomowa)
+    p = momentum(E)
+    F = fermi_function_allowed(Z, E)
+    return p * E * (E0 - E)**2 * F
+
+def compute_R_mean(E):
+    fromMMtoCM = 10
+    # Obliczamy bazowy zasięg dla wody. Korekta na plastik nastąpi w pętli.
+    return (0.108 * E**1.14 / rho_water) * fromMMtoCM
+
+def sample_energies(E0, Z, N): # energia maksymalna, liczba atomowa  i liczba sampli
+    startOfRange = 0.01
+    endOfRange = 5000
+    # TO DO, dodać liczenie maksymalnej energii emisji
+    E_grid = np.linspace(startOfRange, E0, endOfRange)
+    print( E_grid )
+    spectrum = beta_plus_spectrum(E_grid, E0, Z)
+    print( spectrum )
+    cdf = np.cumsum(spectrum)
+    print (cdf)
+    cdf = cdf / cdf[-1]
+    print (cdf)
+    return np.interp(np.random.rand(N), cdf, E_grid)
+
+# -------------------------------------------------------
+# Symulacja Monte Carlo z geometrią sfery (woda -> plastik -> woda)
+# -------------------------------------------------------
+list_of_df = []
+n = np.array([i for i in range(100)])
+for iso, (E0, Z) in isotopes.items():
+    for d in diameters_mm:
+        # DODAĆ SYMULACJĘ EVENT BY EVENT < - niski priorytet
+        R_in = d / 2.0
+        R_out = R_in + wall_thickness_mm
+        
+        # 1. Losowanie pozycji początkowej (tylko w wewnętrznej wodzie)
+        r_pos = R_in * np.random.rand(N_sim)**(1/3)
+        theta_pos = 2 * np.pi * np.random.rand(N_sim)
+        cos_phi_pos = 1 - 2 * np.random.rand(N_sim)
+        sin_phi_pos = np.sqrt(1 - cos_phi_pos**2)
+        
+        x0 = r_pos * sin_phi_pos * np.cos(theta_pos)
+        y0 = r_pos * sin_phi_pos * np.sin(theta_pos)
+        z0 = r_pos * cos_phi_pos
+
+
+        # 2. Losowanie kierunku
+        theta_dir = 2 * np.pi * np.random.rand(N_sim)
+        cos_phi_dir = 1 - 2 * np.random.rand(N_sim)
+        sin_phi_dir = np.sqrt(1 - cos_phi_dir**2)
+        
+        dx = sin_phi_dir * np.cos(theta_dir)
+        dy = sin_phi_dir * np.sin(theta_dir)
+        dz = cos_phi_dir
+        
+
+        # 3. Zasięg bazowy (wodny)
+        E_sampled = sample_energies(E0, Z, N_sim)
+        X_range = compute_R_mean(E_sampled)
+
+        # --- Koniec generacji
+
+        data = {'Index': n, 'Izotop': iso,'Srednica_mm': d,'x': x0, 'y': y0, 'z': z0, 'dx': dx, 'dy': dy, 'dz': dz, 'Energia-wylosowana': E_sampled, 'Range': X_range}
+        df = pd.DataFrame(data)
+
+        list_of_df.append(df)
+
+        n = n+100
+
+
+final_df = pd.concat(list_of_df)
+final_df.to_csv("./dane_symulacja_cal_gonzales/Generacja_danych_c-g.csv", index=False)
+
+# TODO: Inny plik
+#TO DO WYRYSOWAĆ X0, Y0, Z0, r_pos, theta_pos, cos_phi_pos, sin_phi_pos
+# #Jak w :194 zrobić rysunki kontrolne - wektorki kierunku
