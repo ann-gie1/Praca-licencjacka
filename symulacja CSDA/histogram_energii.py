d16b49e65f481549dce0d8ee0cded8c1fa744d74
Ania Gieb <a.giebult@gmail.com>
2026-04-19 21:21:04 +0200

    stworzenie histogramów energii wylosowanych

---
+++ symulacja CSDA/histogram_energii.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import plotly.graph_objects as go
+
+# 1. Wczytanie danych
+plik_wejsciowy = "./dane_symulacja_CSDA/Generacja_danych.csv"
+df = pd.read_csv(plik_wejsciowy)
+
+# 2. Tworzenie histogramu 1D
+fig = go.Figure(data=[go.Histogram(x=df['Energia-wylosowana'])])
+
+# 3. Konfiguracja osi i wyglądu
+fig.update_layout(
+    title='Rozkład energii',
+    xaxis_title='Energia',
+    yaxis_title='Liczba zliczeń'
+)
+
+# 4. Wyświetlenie wykresu
+fig.show()
