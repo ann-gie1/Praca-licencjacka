19df7cb102f30523d2510d7e47fafed9a94cb0cc
Ania Gieb <a.giebult@gmail.com>
2026-04-19 21:09:29 +0200

    dodanie ścieżki na histogramy potwierdzajace dobre losoawnie i stworzenie do tego kodu

--- Symulacja Cal-Gonzales/generacja_danych.py
+++ Symulacja Cal-Gonzales/generacja_danych.py
@@ -51,6 +51,7 @@ def sample_energies(E0, Z, N): # energia maksymalna, liczba atomowa  i liczba sa
 # -------------------------------------------------------
 # Symulacja Monte Carlo z geometrią sfery (woda -> plastik -> woda)
 # -------------------------------------------------------
+list_of_hist_df = []
 list_of_df = []
 global_idx = 0
 for iso, (E0, Z) in isotopes.items():
@@ -68,7 +69,8 @@ for iso, (E0, Z) in isotopes.items():
         x0 = r_pos * sin_phi_pos * np.cos(theta_pos)
         y0 = r_pos * sin_phi_pos * np.sin(theta_pos)
         z0 = r_pos * cos_phi_pos
-
+        
+        phi_pos = np.arccos(cos_phi_pos)
 
         # 2. Losowanie kierunku
         theta_dir = 2 * np.pi * np.random.rand(N_sim)
@@ -78,7 +80,21 @@ for iso, (E0, Z) in isotopes.items():
         dx = sin_phi_dir * np.cos(theta_dir)
         dy = sin_phi_dir * np.sin(theta_dir)
         dz = cos_phi_dir
-        
+            # --- Koniec generacji
+        n = np.arange(global_idx, global_idx + N_sim)
+
+                # Wyciągamy tylko parametry przestrzenne do weryfikacji
+        data_hist = {
+            'Srednica_mm': d,
+            'r': r_pos,
+            'theta': theta_pos,
+            'phi': phi_pos,
+            'x': x0,
+            'y': y0,
+            'z': z0
+        }
+        df_hist = pd.DataFrame(data_hist)
+        list_of_hist_df.append(df_hist)    
 
         # 3. Zasięg bazowy (wodny)
         E_sampled = sample_energies(E0, Z, N_sim)
@@ -96,7 +112,8 @@ for iso, (E0, Z) in isotopes.items():
 
 final_df = pd.concat(list_of_df)
 final_df.to_csv("./dane_symulacja_cal_gonzales/Generacja_danych_c-g.csv", index=False)
-
+final_hist_df = pd.concat(list_of_hist_df)
+final_hist_df.to_csv("./dane_symulacja_cal_gonzales/histogramy_weryfikacja.csv", index=False)
 # TODO: Inny plik
 #TO DO WYRYSOWAĆ X0, Y0, Z0, r_pos, theta_pos, cos_phi_pos, sin_phi_pos
 # #Jak w :194 zrobić rysunki kontrolne - wektorki kierunku
---
+++ Symulacja Cal-Gonzales/histogramy_rozkładu.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+# 1. Wczytanie danych
+plik_wejsciowy = "./dane_symulacja_cal_gonzales/histogramy_weryfikacja.csv"
+df = pd.read_csv(plik_wejsciowy)
+
+# Wybieramy tylko jedną średnicę, żeby r się nie nałożyły z różnych kul
+wybrana_srednica = df['Srednica_mm'].unique()[0]
+df_plot = df[df['Srednica_mm'] == wybrana_srednica]
+
+print(f"Rysowanie histogramów dla średnicy: {wybrana_srednica} mm ({len(df_plot)} cząstek)")
+
+# 2. Utworzenie siatki wykresów 2 wiersze x 3 kolumny
+fig = make_subplots(
+    rows=2, cols=3, 
+    subplot_titles=("Zmienna: r", "Zmienna: Theta (azymut)", "Zmienna: Phi (polarny)", 
+                    "Zmienna: X", "Zmienna: Y", "Zmienna: Z")
+)
+
+# Konfiguracja parametrów dla każdej zmiennej
+zmienne = [
+    ('r', 1, 1, 'blue'),
+    ('theta', 1, 2, 'green'),
+    ('phi', 1, 3, 'purple'),
+    ('x', 2, 1, 'red'),
+    ('y', 2, 2, 'orange'),
+    ('z', 2, 3, 'brown')
+]
+
+# 3. Pętla rysująca
+for nazwa, wiersz, kolumna, kolor in zmienne:
+    fig.add_trace(
+        go.Histogram(
+            x=df_plot[nazwa], 
+            nbinsx=50, 
+            marker_color=kolor, 
+            name=nazwa,
+            showlegend=False
+        ),
+        row=wiersz, col=kolumna
+    )
+    # Dodanie opisów osi
+    fig.update_xaxes(title_text=nazwa, row=wiersz, col=kolumna)
+    fig.update_yaxes(title_text="Zliczenia", row=wiersz, col=kolumna)
+
+# 4. Finalny layout i wyświetlanie
+fig.update_layout(
+    title_text=f"Weryfikacja jednorodności przestrzennej Monte Carlo (Sfera {wybrana_srednica} mm)",
+    height=700, width=1200,
+    template="plotly_white"
+)
+
+fig.show()
