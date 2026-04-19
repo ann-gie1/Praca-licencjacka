4c194d0495eacc779495e39cfac15a9054b167e3
Ania Gieb <a.giebult@gmail.com>
2026-04-19 21:23:49 +0200

    kody do stworzenia kontrolnych histogramów zasięgu

---
+++ Symulacja Cal-Gonzales/histogramy_zasięgu.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import plotly.graph_objects as go
+
+# 1. Wczytanie danych
+plik_wejsciowy = "./dane_symulacja_cal_gonzales/Generacja_danych_c-g.csv"
+df = pd.read_csv(plik_wejsciowy)
+
+# 2. Tworzenie histogramu 1D
+fig = go.Figure(data=[go.Histogram(x=df['Range'])])
+
+# 3. Konfiguracja osi i wyglądu
+fig.update_layout(
+    title='Rozkład zasięgów',
+    xaxis_title='zasięg',
+    yaxis_title='Liczba zliczeń'
+)
+
+# 4. Wyświetlenie wykresu
+fig.show()
