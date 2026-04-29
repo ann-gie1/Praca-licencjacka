0b28ddaef432307ad96d59ea2da05e6281733ada
Ania Gieb <a.giebult@gmail.com>
2026-04-29 17:55:05 +0200

    poprawna zeby poszło na serwerze cz.3

--- Symulacja Cal-Gonzales/histogram_energii.py
+++ Symulacja Cal-Gonzales/histogram_energii.py
@@ -1,5 +1,7 @@
 import pandas as pd
-import plotly.graph_objects as go
+import matplotlib
+matplotlib.use('Agg') # Kluczowe ustawienie dla serwera w tle
+import matplotlib.pyplot as plt
 
 # 1. Wczytanie danych
 plik_wejsciowy = "../dane_symulacja_cal_gonzales/Generacja_danych_c-g_1mln.csv"
@@ -7,15 +9,20 @@ plik_wyjsciowy = "../dane_symulacja_cal_gonzales/histogram_energii_1mln.png"
 df = pd.read_csv(plik_wejsciowy)
 
 # 2. Tworzenie histogramu 1D
-fig = go.Figure(data=[go.Histogram(x=df['Energia-wylosowana'])])
+plt.figure(figsize=(10, 6))
+plt.hist(df['Energia-wylosowana'], bins=100, edgecolor='black', alpha=0.75)
 
 # 3. Konfiguracja osi i wyglądu
-fig.update_layout(
-    title='Rozkład energii',
-    xaxis_title='Energia',
-    yaxis_title='Liczba zliczeń'
-)
+plt.title('Rozkład energii', fontsize=14)
+plt.xlabel('Energia', fontsize=12)
+plt.ylabel('Liczba zliczeń', fontsize=12)
+plt.grid(axis='y', alpha=0.5)
 
-# 4. Wyświetlenie wykresu
-fig.write_image(plik_wyjsciowy)
+# 4. Zapis do pliku
+plt.tight_layout()
+plt.savefig(plik_wyjsciowy, dpi=300)
+
+# Zwolnienie pamięci RAM
+plt.close()
+
 print(f"Gotowe. Wykres zapisano w: {plik_wyjsciowy}")
